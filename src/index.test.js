cfe381e723456faf3ef8fd74542f0c58b744c49a
David Conner <rdconner@us.ibm.com>
2022-03-24 12:34:44 -0400

    fix act warnings in test, change main entry

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,28 @@
-import React from "react";
-import { mount } from "enzyme";
-import { useLangDirection } from ".";
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { mount } from 'enzyme';
+import { useLangDirection } from '.';
 
 const Component = () => {
   const langDir = useLangDirection();
   return <input dir={langDir} type="text" />;
 };
 test("Direction should be 'ltr'", () => {
-  const wrapper = mount(<Component />);
+  let wrapper;
+  act(() => {
+    wrapper = mount(<Component />);
+  });
 
-  expect(wrapper.find("input").prop("dir")).toEqual("ltr");
+  expect(wrapper.find('input').prop('dir')).toEqual('ltr');
 });
 
-test("Direction should be 'rtl'", () => {
-  document.documentElement.setAttribute("dir", "rtl");
-  const wrapper = mount(<Component />);
+test("Direction should be 'rtl'", async () => {
+  let wrapper;
+  await act(async () => {
+    await document.documentElement.setAttribute('dir', 'rtl');
+    wrapper = mount(<Component />);
+  });
 
-  expect(wrapper.find("input").prop("dir")).toEqual("rtl");
+  const inputDir = wrapper.find('input').prop('dir');
+  expect(inputDir).toEqual('rtl');
 });
